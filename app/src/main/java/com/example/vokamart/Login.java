209d1e9fdb893c3d935041e74324946ff1bb50a0
LooseGrip8 <dzikrathoriq673@gmail.com>
2023-10-23 10:54:16 +0700

    bug fixes

--- app/src/main/java/com/example/vokamart/Login.java
+++ app/src/main/java/com/example/vokamart/Login.java
@@ -2,7 +2,10 @@ package com.example.vokamart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Login extends AppCompatActivity {
@@ -10,5 +13,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnlogin = findViewById(R.id.btn_Login);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, MainDashboard.class);
+                startActivity(i);
+            }
+        });
     }
 }
