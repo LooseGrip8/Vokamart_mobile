b1bc31a6d5b044f7b35d6f0b973d384ea3871fcd
LooseGrip8 <dzikrathoriq673@gmail.com>
2023-10-23 10:17:39 +0700

    tab layout is now operable with swipe motion, working on clickable one

--- app/src/main/java/com/example/vokamart/pesanan.java
+++ app/src/main/java/com/example/vokamart/pesanan.java
@@ -1,19 +1,55 @@
 package com.example.vokamart;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import androidx.viewpager2.widget.ViewPager2;
 
 public class pesanan extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+    private ViewPageAdapter adapter;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.pesanan, container, false);
+        View view = inflater.inflate(R.layout.pesanan, container, false);
+
+        tabLayout = view.findViewById(R.id.TabLayoutPesanan);
+        viewPager = view.findViewById(R.id.viewPager);
+
+        adapter = new ViewPageAdapter(requireActivity());
+
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) ->  {
+            switch (position){
+                case 0:
+                    tab.setText("Pesanan Baru");
+                    break;
+                case 1:
+                    tab.setText("Perlu Dikirim");
+                    break;
+                case 2:
+                    tab.setText("Dikirim");
+                    break;
+                case 3:
+                    tab.setText("Selesai");
+                    break;
+                case 4:
+                    tab.setText("Dikomplain");
+                    break;
+            }
+        }).attach();
+
+        return view;
     }
 }
