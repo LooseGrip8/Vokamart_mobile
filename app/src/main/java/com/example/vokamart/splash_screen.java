79634b90d59b7ea84120fdce540aa1537ecdad74
Adi Hendra <adihendra1303@gmail.com>
2023-10-24 21:00:47 +0700

    splash screen

---
+++ app/src/main/java/com/example/vokamart/splash_screen.java
@@ -0,0 +1,29 @@
+package com.example.vokamart;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class splash_screen extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash_screen.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
