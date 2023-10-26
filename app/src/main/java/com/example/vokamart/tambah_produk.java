4e600a39c9e942fb8865088800d933ff704fdd22
Adi Hendra <adihendra1303@gmail.com>
2023-10-26 08:21:30 +0700

    fixed

--- app/src/main/java/com/example/vokamart/tambah_produk.java
+++ app/src/main/java/com/example/vokamart/tambah_produk.java
@@ -11,7 +11,7 @@ public class tambah_produk extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tambah_produk);
+        setContentView(R.layout.activity_produk_tambah_produk);
 
     }
 }
